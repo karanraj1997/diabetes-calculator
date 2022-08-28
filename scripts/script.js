842ef8d87b4290a8c70447c5f1d678fef7466763
Karanraj Duraikannu <karanraj.duraikannu@orionic.onmicrosoft.com>
2022-08-28 18:30:54 +0530

    fix: add calculation value

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,171 @@ const prevBtn = document.getElementById("prev-btn")
 const resultDiv = document.getElementById("result-div")
 const questionsListDiv = document.getElementsByClassName("question")
 const smokingSubQuestions = document.getElementById("is_smoking_yes_options")
+const progressBar = document.getElementById("progress-bar-div")
 
 let currentQuestion = 0
+
+const valueCalculation = {
+  age: (value, allValues) => {
+    if (value > 0 && value <= 10) {
+      return 10
+    }
+    else if (value > 10 && value <= 30) {
+      return 5
+    }
+    return 2
+  },
+
+  gender: (value, allValues) => {
+    if (value === "male") return 5
+    if (value === "female") return 7
+  },
+  height: (value, allValues) => {
+    if (value < 150) {
+      return 10
+    }
+    return 5
+  },
+  weight: (value, allValues) => {
+    if (value <50) {
+      return 8
+    }
+    if (value < 80) {
+      return 10
+    }
+    return 5
+  },
+  ethnicity: (value, allValues) => {
+    if (value === "asian") {
+      return 10
+    }
+    if (value === "europeans") {
+      return 10
+    }
+    if (value === "pacific-islands") {
+      return 2
+    }
+    return 0
+  },
+  is_smoking: (value, allValues) => {
+    let total = 10
+    if (value=="yes") {
+      if (allValues["sticks_per_day_work"] > 10) {
+        total -= 4
+      }
+      if (allValues["sticks_per_day_home"]) {
+        total -= 4
+      }
+      if (allValues["sticks_per_day_other"]) {
+        total -= 4
+      }
+    } else {
+      total = total - 5
+    }
+    return total
+  },
+  vigorous: (value) => {
+    if (value === "none") {
+      return 10
+    }
+    if (value === "less_10") {
+      return 8
+    }
+    if (value === "10to30") {
+      return 6
+    }
+    if (value === "31to50") {
+      return 4
+    }
+    if (value === "51to75") {
+      return 2
+    }
+    return 0
+  },
+  moderate: (value) => {
+    if (value === "none") {
+      return 10
+    }
+    if (value === "<10") {
+      return 8
+    }
+    if (value === "10>50") {
+      return 6
+    }
+    if (value === "51>90") {
+      return 4
+    }
+    if (value === "91>150") {
+      return 2
+    }
+    return 0
+  },
+  walking: (value) => {
+    if (value === "none") {
+      return 0
+    }
+    if (value === "10to75") {
+      return 2
+    }
+    if (value === "76to150") {
+      return 4
+    }
+    if (value === "151to225") {
+      return 6
+    }
+    if (value === "226to300") {
+      return 8
+    }
+    return 10
+  },
+  parents_have_diabetes: (value) => {
+    if (value === "yes") {
+      return 5
+    }
+    return 10
+  },
+  have_hypertension: (value) => {
+    if (value === "yes") {
+      return 5
+    }
+    return 10
+  },
+  symptoms: (value) => {
+    let total = 10
+    if (value.includes("increased thirst")) {
+      total -= 2
+    }
+    if (value.includes("frequent urination")) {
+      total -= 2
+    }
+    if (value.includes("extreme hunger")) {
+      total -= 2
+    }
+    if (value.includes("unexplained weight loss")) {
+      total -= 2
+    }
+    if (value.includes("slow healing sores")) {
+      total -= 2
+    }
+    if (value.includes("frequent infections")) {
+      total -= 2
+    }
+    if (value.includes("numbness or tingling")) {
+      total -= 2
+    }
+    return total
+  }
+}
+
+
+
+// const allInputs = document.getElementsByTagName("section")[0].getElementsByTagName('input')
+// let i = 0
+// while (allInputs.length > i) {
+//   const inputName = allInputs[i].getAttribute("name")
+//   console.log(inputName, "<===>", allInputs[i].value)
+//   i++
+// }
 
 
 const updateValue = (event) => {
@@ -32,7 +195,7 @@ const updateValue = (event) => {
     if (name === "is_smoking") {
       if (event.target.value === "yes" && smokingSubQuestions.classList.contains("display-none")) {
         smokingSubQuestions.classList.remove("display-none")
-      } else if (!smokingSubQuestions.classList.contains("display-none")){
+      } else if (!smokingSubQuestions.classList.contains("display-none")) {
         smokingSubQuestions.classList.add("display-none")
       }
     }
@@ -44,7 +207,7 @@ const getCurrentQuestionFieldNames = () => {
   const childInputs = questionsListDiv[currentQuestion].getElementsByTagName("input")
   let index = 0
   const names = []
-  while(index < childInputs.length) {
+  while (index < childInputs.length) {
     const name = childInputs[index].getAttribute("name")
     if (!names.includes(name)) {
       names.push(name)
@@ -92,12 +255,20 @@ const canSubmit = () => {
   return isValid
 }
 
+const updateProgressBar = () => {
+  const currentValue = (currentQuestion / questionsListDiv.length) * 100
+  progressBar.style = "width: " + currentValue + "%;"
+  progressBar.setAttribute("aria-valuenow", currentValue)
+  progressBar.setAttribute("aria-valuenow", currentValue)
+}
+
 const updatePageInfo = () => {
   if (currentQuestion === 0) {
     prevBtn.setAttribute("disabled", "true")
   } else {
     prevBtn.removeAttribute("disabled")
   }
+  updateProgressBar()
   pageInfoDiv.innerHTML = (currentQuestion + 1) + "/" + questionsListDiv.length
 }
 
@@ -127,7 +298,15 @@ const showCurrentQuestion = () => {
 }
 const handleSubmit = () => {
   hideAllQuestions()
+  updateProgressBar()
   resultDiv.classList.remove("display-none")
+  const percentage = document.getElementById("percentage")
+  let totalValue = 0
+  Object.keys(formValues).forEach((key) => {
+    totalValue += valueCalculation[key]?.(formValues[key], formValues) || 0
+  })
+
+  percentage.innerHTML = totalValue
   pageInfoDiv.innerHTML = "Submitted"
 }
 const handleNext = () => {
